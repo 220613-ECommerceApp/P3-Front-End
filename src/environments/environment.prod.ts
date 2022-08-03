44e478dc18d626015a1837aed9c725553c83fb50
William Moore <wilchasemoore@gmail.com>
2022-08-03 14:47:10 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  withCredentials: true,
+  baseUrl: 'http://propanegaming-env.eba-khy74pa3.us-east-1.elasticbeanstalk.com',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': ''
+  }
 };
