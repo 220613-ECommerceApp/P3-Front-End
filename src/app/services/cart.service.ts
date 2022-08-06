7943aed152c96fed489a514d321ca0120d23c5b7
ali alrubaye <alialrubaye1990@hotmail.com>
2022-08-06 01:16:32 -0400

    saved the session in cart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -4,20 +4,20 @@ import { Subject, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Cartitem } from '../models/cartitem';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
   subject: Subject<Cartitem[]> = new Subject<Cartitem[]>();
   //fetch cartItem table for logged-in user
   getCart(): void {
+    this.auth.updateBearer();
     this.http
-      .get<Cartitem[]>(environment.baseUrl + '/api/cart', {
-        headers: environment.headers,
-      })
+      .get<Cartitem[]>(environment.baseUrl + '/api/cart', {headers: environment.headers,})
       .pipe(
         catchError((e) => {
           return throwError(e);
