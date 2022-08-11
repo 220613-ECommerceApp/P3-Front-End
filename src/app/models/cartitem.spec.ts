dadca21c4c0da6c487b7439e5249a418bd3f4f64
Yuriy Koziy <yukoziy@gmail.com>
2022-08-11 17:10:13 -0400

    fixing model tests because cant run any tests

--- src/app/models/cartitem.spec.ts
+++ src/app/models/cartitem.spec.ts
@@ -1,7 +1,16 @@
 import { Cartitem } from './cartitem';
+import { Product } from './product';
+import { User } from './user';
 
 describe('Cartitem', () => {
   it('should create an instance', () => {
-    expect(new Cartitem()).toBeTruthy();
+    expect(
+      new Cartitem(
+        1,
+        12,
+        new Product(1, 'hat', 12, 'bryans hat', 99.99, 'hat url'),
+        new User(1, 'Bob', 'Who', 'testUser', '123456', 'testemail@gmail.com')
+      )
+    ).toBeTruthy();
   });
 });
