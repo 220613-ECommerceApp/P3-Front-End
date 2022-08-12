a21f377a28ad4b093675d7d9f87e38157d756285
Yuriy Koziy <yukoziy@gmail.com>
2022-08-12 09:31:18 -0400

    testing search bar on init and search icon click

--- src/app/components/search/search-bar/search-bar.component.spec.ts
+++ src/app/components/search/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Type } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SearchService } from 'src/app/services/search.service';
 
 import { SearchBarComponent } from './search-bar.component';
 
@@ -18,10 +25,30 @@ describe('SearchBarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getTags on init', () => {
+    let service = fixture.debugElement.injector.get<SearchService>(
+      SearchService as Type<SearchService>
+    );
+    let serviceSpy = spyOn(service, 'getTags').and.callThrough();
+    component.ngOnInit();
+    expect(serviceSpy).toHaveBeenCalled();
+  });
+
+  it('should call searchClicked when search icon clicked', fakeAsync(() => {
+    spyOn(component, 'searchClicked');
+
+    let button =
+      fixture.debugElement.nativeElement.querySelector('.search-btn');
+    button.click();
+    tick();
+    expect(component.searchClicked).toHaveBeenCalled();
+  }));
 });
