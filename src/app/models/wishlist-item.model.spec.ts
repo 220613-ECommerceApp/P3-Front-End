87a2ce90e27f0b46de7ac70be2f7bd730e64897e
Yuriy Koziy <yukoziy@gmail.com>
2022-08-13 08:09:24 -0400

    Fix all failing tests (that would not compile), including wishlist tests

--- src/app/models/wishlist-item.model.spec.ts
+++ src/app/models/wishlist-item.model.spec.ts
@@ -1,7 +1,13 @@
+import { Product } from './product';
 import { WishlistItem } from './wishlist-item';
 
 describe('WishlistItem', () => {
   it('should create an instance', () => {
-    expect(new WishlistItem()).toBeTruthy();
+    expect(
+      new WishlistItem(
+        1,
+        new Product(1, 'hat', 12, 'bryans hat', 99.99, 'hat url')
+      )
+    ).toBeTruthy();
   });
 });
--- src/app/services/wishlist.service.spec.ts
+++ src/app/services/wishlist.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { WishlistService } from './wishlist.service';
@@ -6,7 +7,9 @@ describe('WishlistService', () => {
   let service: WishlistService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(WishlistService);
   });
 
