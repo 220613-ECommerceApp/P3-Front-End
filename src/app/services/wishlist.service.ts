d9b810e6be050df2d7ca09c14e417710d4320933
Anthony Lopez <28433525+tlb99@users.noreply.github.com>
2022-08-12 17:05:59 -0700

    implemented addToWishlist method in wishlist.service.ts

--- src/app/services/wishlist.service.ts
+++ src/app/services/wishlist.service.ts
@@ -1,25 +1,40 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { WishlistItem } from '../models/wishlist-item';
 import { AuthService } from './auth.service';
 
-interface Wishlist{
-  wishlistItems: WishlistItem[];
-}
-
 @Injectable({
   providedIn: 'root'
 })
-
 export class WishlistService {
-  private getWishlistUrl: string = `/api/getWishList`;
 
   constructor(private http: HttpClient, private auth: AuthService) { }
 
-  public getWishlistItems(): Observable<WishlistItem[]>{
+  getWishlistItems(): Observable<WishlistItem[]>{
     this.auth.updateBearer()
-    return this.http.get<WishlistItem[]>(environment.baseUrl + this.getWishlistUrl, {headers: environment.headers});
+    return this.http.get<WishlistItem[]>(environment.baseUrl + `/api/getWishList`, {headers: environment.headers});
+  }
+
+  addToWishlist(ProductId: {productId: number}){
+    this.auth.updateBearer();
+    this.http
+    .post<{productId: number}>(
+      environment.baseUrl + `/api/addToWishList`,
+      JSON.stringify(ProductId),
+      {
+        headers: new HttpHeaders({
+          Authorization: environment.headers.Authorization,
+          'Content-Type': environment.headers['Content-Type'],
+        }),
+      }
+    )
+    .pipe(
+      catchError((e) => {
+        return throwError(e);
+      })
+    ).subscribe();
   }
 }
