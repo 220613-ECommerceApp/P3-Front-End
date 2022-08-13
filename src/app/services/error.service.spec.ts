8a0cca85ded980868a9302aa75b119438d49bd20
Yuriy Koziy <yukoziy@gmail.com>
2022-08-12 20:08:01 -0400

    Error service tests 100%

--- src/app/services/error.service.spec.ts
+++ src/app/services/error.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ErrorService } from './error.service';
 
 describe('ErrorService', () => {
@@ -12,5 +11,32 @@ describe('ErrorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set message using setMessage', () => {
+    ErrorService.setMessage('test msg');
+    expect(ErrorService.message).toEqual('test msg');
+  });
+
+  it('should set alertWarning using displayWarning', () => {
+    ErrorService.displayWarning(true);
+    expect(ErrorService.alertWarning).toEqual(true);
+    expect(ErrorService.alertSuccess).toEqual(false);
+  });
+
+  it('should set alertSuccess using displaySuccess', () => {
+    ErrorService.displaySuccess(true);
+    expect(ErrorService.alertSuccess).toEqual(true);
+    expect(ErrorService.alertWarning).toEqual(false);
+  });
+
+  it('should set alertWarning to false using closeAlertWarning', () => {
+    ErrorService.closeAlertWarning();
+    expect(ErrorService.alertWarning).toEqual(false);
+  });
+
+  it('should set alertSuccess to false using closeAlertSuccess', () => {
+    ErrorService.closeAlertSuccess();
+    expect(ErrorService.alertSuccess).toEqual(false);
   });
 });
